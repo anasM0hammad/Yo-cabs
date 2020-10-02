2f12bc9fd318984abee40270cbeabcb76808bae5
Areesha Fatima Siddiqui <areeshaf7@gmail.com>
2020-10-02 16:36:56 +0530

    updated connection error variable

--- php/customer.php
+++ php/customer.php
@@ -69,7 +69,8 @@ if (mysqli_query($conn, $sql)) {
 
 
 } else {
-    echo "Error insertin data: " . mysqli_error($conn);
+    $connErr = mysqli_error($conn);
+    echo "Error insertin data: " . $connErr;
 }
 
 mysqli_close($conn);
